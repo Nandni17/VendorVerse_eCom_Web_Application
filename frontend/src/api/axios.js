81b470e108681d1b9475ff9578d9226b2a62f714
Nandni17 <sp24bsse0017@maju.edu.pk>
2026-08-22 23:30:52 +0500

    update url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: "http://localhost:8000",
-});
-
 // const API = axios.create({
-//   baseURL: "https://vendor-verse-e-com-web-application.vercel.app/",
+//   baseURL: "http://localhost:8000",
 // });
+
+const API = axios.create({
+  baseURL: "https://vendor-verse-e-com-web-application.vercel.app/",
+});
 
 API.interceptors.request.use((req) => {
   const storedUser = localStorage.getItem("user");
